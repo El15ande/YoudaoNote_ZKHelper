b2d7991f2a53d5dd4ee6c47b90ee81bae60d0a82
31iSande <1140076527@qq.com>
2021-02-11 01:09:02 +0000

    Ver. 0.2.0 Add B-card & N-card buttons

---
+++ popup.js
@@ -0,0 +1,37 @@
+document.getElementById('bcardbtn').onclick = function () {
+    let _index = document.getElementById('bcardidx').value;
+
+    navigator.clipboard.writeText(
+`#### <center>${_index ? _index : '1.1-1'}
+    
+**Tag**
+
+**Note**
+
+**Resource**`
+    )
+    .then(() => console.log("newcard copied"))
+    .catch(() => console.log("newcard copy failed"));
+}
+
+document.getElementById('ncardbtn').onclick = function () {
+    let _now = new Date();
+    let _month = _now.getMonth() + 1;
+    let _day = _now.getDate();
+
+    let makeString = (d) => {
+        return (d < 10) ? `0${d}` : `${d}`;
+    }
+
+    navigator.clipboard.writeText(
+`#### <center>${makeString(_month)}-${makeString(_day)}
+    
+**Tag**
+
+**Note**
+
+**Resource**`
+    )
+    .then(() => console.log("newcard copied"))
+    .catch(() => console.log("newcard copy failed"));
+}
